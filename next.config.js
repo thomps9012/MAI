b903b9c182af27513f203ab9f8fba9996c323f58
Samuel Thompson <thomps9012@gmail.com>
2022-03-01 12:55:40 -0500

    built out application and imported base level package json

---
+++ next.config.js
@@ -0,0 +1,9 @@
+export async function redirects() {
+    return [
+        {
+            source: '/',
+            destination: 'interview_select',
+            permanent: true
+        }
+    ]
+}
