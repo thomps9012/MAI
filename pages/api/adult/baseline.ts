74da17a5fc72fc47bfce9a2ea5cbbecfed0fa53b
Samuel Thompson <thomps9012@gmail.com>
2022-03-03 11:16:52 -0500

    updated api calls

--- pages/api/adult/baseline.ts
+++ pages/api/adult/baseline.ts
@@ -3,6 +3,6 @@ import { connectToDatabase } from '../../../utils/mongodb';
 export default async function handler(req: {body: string;}, res: {json: (arg0: any) => void;}){
     const {db} = await connectToDatabase();
     let data = JSON.parse(req.body);
-    const response = await db.collection('adult_baselines').insertOne({gift_card_received: true, ...data});
+    const response = await db.collection('adult_baselines').insertOne({gift_card_received: false, ...data});
     res.json(response);
 }
