5ee92d18fa470efd1156fa23c6ccac6f81b14bb5
Samuel Thompson <thomps9012@gmail.com>
2022-03-04 12:51:03 -0500

    much success

--- utils/stateChecker.tsx
+++ utils/stateChecker.tsx
@@ -1,17 +1,16 @@
 function StateChecker(pageState: any) {
     const unansweredArray = [];
     for (const item in pageState) {
-        console.log(pageState[item])
-        console.log(document.querySelector('button'))
         if (pageState[item] === '') {
             unansweredArray.push(pageState[item])
         }
-        if (unansweredArray.length != 0) {
-            document.querySelector('button')?.setAttribute('disabled', 'true')
-        } else {
-            document.querySelector('button')?.removeAttribute('disabled')
-        }
     }
+    if (unansweredArray.length != 0) {
+        document.querySelector('button')?.setAttribute('disabled', 'true')
+    } else {
+        document.querySelector('button')?.removeAttribute('disabled')
+    }
+
 }
 
 export default StateChecker;
