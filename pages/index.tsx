d532cf7b12a9f523e97673576a5a8e881722f169
Samuel Thompson <thomps9012@gmail.com>
2022-03-15 11:07:20 -0400

    container styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,7 +57,7 @@ export default function InterviewSelect(interviewCounts: any) {
     }
 
     return (
-        <div style={{ display: 'flex', flexDirection: 'column' }} className='interviewSelect'>
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }} className='interviewSelect'>
             <div className="interviewInput">
                 <h2>Select Interview Type</h2>
                 <select onChange={(e: any) => setInterview(e.target.value)}>
