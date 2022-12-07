722ca186875a745ba41bb1b1fbad1b9236b5c2cf
Samuel Thompson <thomps9012@gmail.com>
2022-12-07 15:08:25 -0500

    removal of developmental features

--- pages/api/user/login.ts
+++ pages/api/user/login.ts
@@ -1,99 +1,24 @@
 import { connectToDatabase } from "../../../utils/mongodb";
-import { setCookie } from "cookies-next";
 const bcrypt = require("bcrypt");
 export default async function handler(req: any, res: any) {
   const { db } = await connectToDatabase();
-  const ip_address = await fetch("https://jsonip.com/").then((res) =>
-    res.json()
-  );
   const data = JSON.parse(req.body);
-  const { login_attempts, email, password, username } = data;
-  console.log(data);
-  setCookie("current_ip_address", ip_address.ip);
-  console.log(ip_address.ip)
-  if (login_attempts >= 10) {
-    const current_hours = new Date().getHours();
-    const result1 = await db.collection("users").updateOne(
-      { email: email },
-      {
-        account_locked: true,
-        lock_expiration: new Date().setHours(current_hours + 6),
-      }
-    );
-    const result2 = await db.collection("users").updateOne(
-      { username: username },
-      {
-        account_locked: true,
-        lock_expiration: new Date().setHours(current_hours + 6),
-      }
-    );
-    if (result1 || result2) {
-      res.json({
-        error:
-          "too many unsuccessful login attempts, your account has been locked for six hours",
-      });
-    }
-  }
+  const { email, password, username } = data;
   if (email != null && email != undefined && email != "") {
     const result = await db.collection("users").findOne({ email: email });
-    if (!result) {
-      setCookie("login_attempts", login_attempts + 1, {
-        req,
-        res,
-      });
-      res.json({ error: "incorrect login credentials" });
-    }
     const check_pw = await bcrypt.compare(password, result.password);
-    if (!check_pw) {
-      setCookie("login_attempts", login_attempts + 1, {
-        req,
-        res,
-      });
+    if (!result || !check_pw) {
       res.json({ error: "incorrect login credentials" });
-    }
-    if (result && check_pw) {
-      setCookie("login_attempts", 0);
-    }
-    if (result.account_locked) {
-      res.json({
-        error:
-          "your account has been locked after too many unsuccessful attempts",
-      });
     }
     res.json(result);
   } else if (username != null && username != undefined && username != "") {
     const result = await db.collection("users").findOne({ username: username });
-    console.log(result);
-    if (!result) {
-      res.json({ error: "incorrect login credentials" });
-      setCookie("login_attempts", JSON.stringify(login_attempts + 1), {
-        req,
-        res,
-      });
-    }
     const check_pw = await bcrypt.compare(password, result.password);
-    if (!check_pw) {
-      setCookie("login_attempts", login_attempts + 1, {
-        req,
-        res,
-      });
+    if (!result || !check_pw) {
       res.json({ error: "incorrect login credentials" });
-    }
-    if (result.account_locked) {
-      res.json({
-        error:
-          "your account has been locked after too many unsuccessful attempts",
-      });
     }
     res.json(result);
-    if (result && check_pw) {
-      setCookie("login_attempts", 0);
-    }
   } else {
-    setCookie("login_attempts", login_attempts + 1, {
-      req,
-      res,
-    });
     res.json({ error: "incorrect login credentials" });
   }
 }
--- pages/sign_in.tsx
+++ pages/sign_in.tsx
@@ -1,19 +1,6 @@
 import { useRouter } from "next/router";
-import { getCookie, setCookie } from "cookies-next";
-export async function getServerSideProps({ req, res }: any) {
-  const login_attempts = getCookie("login_attempts", { req, res }) as string;
-  return {
-    props: { login_attempts: login_attempts ? parseInt(login_attempts) : 0 },
-  };
-}
-export default function SignIn({ login_attempts }: { login_attempts: any }) {
-  if (login_attempts > 10) {
-    return (
-      <main>
-        <h1>Access Denied Due to an Inordinate Amount of Login Attempts</h1>
-      </main>
-    );
-  }
+import { setCookie } from "cookies-next";
+export default function SignIn() {
   const router = useRouter();
   const valid_password = () => {
     const pw = (document.querySelector(".pw") as HTMLInputElement).value;
@@ -56,30 +43,9 @@ export default function SignIn({ login_attempts }: { login_attempts: any }) {
     if (!valid_password() || !validate_user_id()) {
       return;
     }
-    let device = "Unknown Device";
-    if (navigator.userAgent.indexOf("Win") != -1) {
-      device = "Windows Device";
-    }
-    if (navigator.userAgent.indexOf("Mac") != -1) {
-      device = "Mac Device";
-    }
-    if (navigator.userAgent.indexOf("Linux") != -1) {
-      device = "Linux Device";
-    }
-    if (navigator.userAgent.indexOf("Android") != -1) {
-      device = "Android Device";
-    }
-    if (
-      navigator.userAgent.indexOf("Android") != -1 &&
-      navigator.userAgent.indexOf("Linux") != -1
-    ) {
-      device = "Android/Linux Device";
-    }
-    console.log(login_attempts);
     const user_res = await fetch("/api/user/login", {
       method: "POST",
       body: JSON.stringify({
-        login_attempts,
         email,
         username: userName,
         password: firstPW,
@@ -88,23 +54,8 @@ export default function SignIn({ login_attempts }: { login_attempts: any }) {
     if (user_res.error) {
       alert(user_res.error);
       router.reload();
-      // if (login_attempts === NaN) {
-      //   setCookie("login_attempts", 1);
-      //   router.reload();
-      //   return;
-      // }
-      // setCookie("login_attempts", login_attempts++);
-      // router.reload();
       return;
     }
-    const device_info = await fetch("/api/user/device_info", {
-      headers: { device: device, user_info: JSON.stringify(user_res) },
-    }).then((res) => res.json());
-    device_info &&
-      (await fetch("/api/user/email_smtp", {
-        method: "POST",
-        body: JSON.stringify(device_info),
-      }));
     const { admin, editor, _id, full_name } = user_res;
     setCookie("user_admin", admin);
     setCookie("user_editor", editor);
